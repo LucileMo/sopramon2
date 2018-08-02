c70b5b52312f397a1dd6915f1017b4c55857ac35
Unknown <briac.loyer@gmail.com>
2018-08-02 14:44:20 +0200

    CLASSE MODELE BOSS

--- sopramon-model/src/main/java/sopramon/model/Boss.java
+++ sopramon-model/src/main/java/sopramon/model/Boss.java
@@ -1,15 +1,73 @@
 package sopramon.model;
 
+import java.util.List;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "boss")
+@AttributeOverrides({
+	@AttributeOverride(name="id", column=@Column(name="BOSS_ID")),
+	@AttributeOverride(name="nom", column=@Column(name="BOSS_NOM")),
+	@AttributeOverride(name="signe", column=@Column(name="BOSS_SIGNE")),
+	@AttributeOverride(name="niveau", column=@Column(name="BOSS_NIVEAU")),
+	@AttributeOverride(name="capacite", column=@Column(name="BOSS_ADRESSE"))
+})	
+
 public class Boss implements ICombattant {
 	
+		
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "BOSS_ID")
 	public int id;
-	public String nom;
-	public String type;
+
+	@Column(name = "BOSS_NOM", length = 45, nullable = false)
+	@NotEmpty
+	@Size(max = 50)
+	public String nom;	
+
+	@Column(name = "BOSS_NIVEAU", length = 50, nullable = false)
+	@NotEmpty
+	@Size(max = 50)
 	public int niveau;
-	public Capacite capacite;
 	
+	//CLE ETRANGERE
+	@OneToOne
+	@JoinColumn(name="BOSS_CAPACITE_ID")
+	private Capacite capacite;
 	
+	//CLE ETRANGERE
+	@OneToMany(mappedBy="boss")
+	private List<Combat> combat;
 	
+	@Column(name = "BOSS_SIGNE", length = 45, nullable = false)
+	@NotEmpty
+	@Size(max = 50)
+	public Signe signe;	
+	
+	public Signe getSigne() {
+		return signe;
+	}
+
+	public void setSigne(Signe signe) {
+		this.signe = signe;
+	}
+
 	public int getId() {
 		return id;
 	}
@@ -26,12 +84,13 @@ public class Boss implements ICombattant {
 		this.nom = nom;
 	}
 
-	public String getType() {
-		return type;
+
+	public List<Combat> getCombat() {
+		return combat;
 	}
 
-	public void setType(String type) {
-		this.type = type;
+	public void setCombat(List<Combat> combat) {
+		this.combat = combat;
 	}
 
 	public int getNiveau() {
@@ -49,7 +108,6 @@ public class Boss implements ICombattant {
 	public void setCapacite(Capacite capacite) {
 		this.capacite = capacite;
 	}
-
 
 	public void attaquer() {
 		// TODO Auto-generated method stub
