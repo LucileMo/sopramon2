2598e0331d11cce4a71a671e43186043163b1e5f
LucileMo <lucile.morin@outlook.fr>
2018-08-02 15:12:06 +0200

    Classes Achat, Prix, Sopra

--- sopramon-model/src/main/java/sopramon/model/Item.java
+++ sopramon-model/src/main/java/sopramon/model/Item.java
@@ -1,14 +1,43 @@
 package sopramon.model;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+
+
 public class Item {
-	
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name="ACH_ID")	
 private int id;
+@Column(name="ITEM_NOM")
+@NotEmpty
+@Size(max=45)
 private String nom;
+@Column(name="ITEM_PRIX", nullable=false)
+@Positive
 private Double prix;
-private Capacite capacite;
+@Column(name="ITEM_CAPACITE", nullable=false)
+private String capacite;
+@OneToMany(mappedBy="Item")
+private List<Achat> achats;
 
 
 
+public List<Achat> getAchats() {
+	return achats;
+}
+public void setAchats(List<Achat> achats) {
+	this.achats = achats;
+}
 public int getId() {
 	return id;
 }
@@ -27,13 +56,12 @@ public Double getPrix() {
 public void setPrix(Double prix) {
 	this.prix = prix;
 }
-public Capacite getCapacite() {
+public String getCapacite() {
 	return capacite;
 }
-public void setCapacite(Capacite capacite) {
+public void setCapacite(String capacite) {
 	this.capacite = capacite;
 }
-
 
 
 }
--- sopramon-model/src/main/java/sopramon/model/Sopramon.java
+++ sopramon-model/src/main/java/sopramon/model/Sopramon.java
@@ -1,16 +1,48 @@
 package sopramon.model;
 
 import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.PositiveOrZero;
 
 public class Sopramon extends Utilisateur implements ICombattant {
+	
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name="SOP_ID")
 private int id;
+@Column(name="SOP_DATE")
+@Temporal(TemporalType.TIMESTAMP)
+private java.util.Date Date;
 private Date dateNaissance;
+@Column(name="SOP_EXPERIENCE")
+@PositiveOrZero
 private int experience;
+@Column(name="SOP_NIVEAU")
+@PositiveOrZero
 private int niveau;
+@Column(name="SOP_ARGENT")
+@PositiveOrZero
 private double argent;
+//lien capacite
 private Capacite capacite;
+//lien signe
 private Signe signe;
-private int utilisateur;
+//lien utilisateur
+private Utilisateur utilisateur;
+@OneToMany(mappedBy="Sopramon")
+private List<Achat> achats;
+@OneToMany(mappedBy="Sopramon")
+private List<Combat> combats;
+
 
 
 public int getId() {
@@ -57,11 +89,31 @@ public Signe getSigne() {
 public void setSigne(Signe signe) {
 	this.signe = signe;
 }
-public int getUtilisateur() {
+
+
+public java.util.Date getDate() {
+	return Date;
+}
+public void setDate(java.util.Date date) {
+	Date = date;
+}
+public Utilisateur getUtilisateur() {
 	return utilisateur;
 }
-public void setUtilisateur(int utilisateur) {
+public void setUtilisateur(Utilisateur utilisateur) {
 	this.utilisateur = utilisateur;
+}
+public List<Achat> getAchats() {
+	return achats;
+}
+public void setAchats(List<Achat> achats) {
+	this.achats = achats;
+}
+public List<Combat> getCombats() {
+	return combats;
+}
+public void setCombats(List<Combat> combats) {
+	this.combats = combats;
 }
 public void attaquer(){
 }
