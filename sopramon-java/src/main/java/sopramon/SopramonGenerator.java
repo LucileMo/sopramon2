37c8e24c5b774286889ec47734ec4f1afa896e83
LucileMo <lucile.morin@outlook.fr>
2018-08-02 17:21:06 +0200

    Auto stash before merge of "master" and "origin/master"

--- sopramon-java/src/main/java/sopramon/SopramonGenerator.java
+++ sopramon-java/src/main/java/sopramon/SopramonGenerator.java
@@ -2,7 +2,11 @@ package sopramon;
 
 import java.util.Date;
 
+import sopramon.DAO.IDAOSopramon;
 import sopramon.DAO.IDAOUtilisateur;
+import sopramon.DAO.Hibernate.DAOSopramonHibernate;
+import sopramon.DAO.Hibernate.DAOUtilisateurHibernate;
+import sopramon.model.Sopramon;
 import sopramon.model.Utilisateur;
 
 
@@ -22,6 +26,21 @@ public class SopramonGenerator {
 		myUtilisateur.setPassword("");
 		
 		daoUtilisateur.save(myUtilisateur);
+		
+		//Ajout d'un Sopramon
+		IDAOSopramon daoSopramon = new DAOSopramonHibernate();
+		Sopramon mySopramon = new Sopramon();
+		
+		mySopramon.setNom("");
+		//mySopramon.setDate(12/05/2000);
+		mySopramon.setExperience(1);
+		mySopramon.setNiveau(1);
+		mySopramon.setArgent(1);
+		//mySopramon.setCapacite(myCapacite);
+		//mySopramon.setSigne(mySigne);
+		
+		daoUtilisateur.save(myUtilisateur);
+		
 		
 		
 		
