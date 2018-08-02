2617144b7f1900fcba47a1d7afccb489b7b92549
Unknown <briac.loyer@gmail.com>
2018-08-02 15:54:33 +0200

    coup cle etrangere

--- sopramon-model/src/main/java/sopramon/model/Coup.java
+++ sopramon-model/src/main/java/sopramon/model/Coup.java
@@ -1,15 +1,24 @@
 package sopramon.model;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "coup")
 
 public class Coup {
 
@@ -32,15 +41,15 @@ public class Coup {
 	private int peristance;
 
 	// CLE ETRANGERE
-
+	@ManyToOne
+	@JoinColumn(name = "COUP_SOPRAMON_ID")
 	private int sopramonid;
 
 	// CLE ETRANGERE
-	private int combatid;
-
-	public Date getDate() {
-		return date;
-	}
+	@ManyToOne
+	@JoinColumn(name = "COUP_COMBAT_ID")
+	private int combat;
+	
 
 	public void setDate(Date date) {
 		this.date = date;
