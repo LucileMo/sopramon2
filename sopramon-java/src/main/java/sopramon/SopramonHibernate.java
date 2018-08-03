bf3e429062d84825d90fa5c7d888b7b904791a3f
LucileMo <lucile.morin@outlook.fr>
2018-08-03 09:20:49 +0200

    hello

--- sopramon-java/src/main/java/sopramon/SopramonHibernate.java
+++ sopramon-java/src/main/java/sopramon/SopramonHibernate.java
@@ -7,29 +7,22 @@ import sopramon.model.Item;
 
 public class SopramonHibernate {
 
-	public static class PrincipalHibernate {
+    public static void main(String[] args) {
+//            printItems();
+//        printFournisseurs();
+//        printProduits();
+//        printProduitByClientId(2);
+//        printProduitByFournisseurId(1);
 
-		public static void main(String[] args) {
-//			printItems();
-//		printFournisseurs();
-//		printProduits();
-//		printProduitByClientId(2);
-//		printProduitByFournisseurId(1);
+//            DAOHibernate.close();
 
-//			DAOHibernate.close();
+    }
 
-			public static void printItems() {
-				IDAOItem daoItem = new DAOItemHibernate();
-				
-				for (Item i : IDAOItem.findAll()) {
-					System.out.println(i.getNom());
-				}
-			}
-		
-			
-			
-	}
+    public static void printItems() {
+        IDAOItem daoItem = new DAOItemHibernate();
 
-	}
-
+        for (Item i : daoItem.findAll()) {
+            System.out.println(i.getNom());
+        }
+    }
 }
