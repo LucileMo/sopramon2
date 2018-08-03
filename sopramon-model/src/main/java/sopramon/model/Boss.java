73c469db7255dfd6f0bd9b7fc2e01aafd84c9aaa
LucileMo <lucile.morin@outlook.fr>
2018-08-03 10:22:03 +0200

    ljsflqlfj

--- sopramon-model/src/main/java/sopramon/model/Boss.java
+++ sopramon-model/src/main/java/sopramon/model/Boss.java
@@ -2,6 +2,7 @@ package sopramon.model;
 
 import java.util.List;
 
+
 import javax.persistence.AttributeOverride;
 import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
--- sopramon-model/src/main/java/sopramon/model/Capacite.java
+++ sopramon-model/src/main/java/sopramon/model/Capacite.java
@@ -1,11 +1,14 @@
 package sopramon.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+
+@Entity
 
 public class Capacite {
 	
