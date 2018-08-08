8b4f0bb3c392cba1e8511a41a03eec79b56b28da
Unknown <briac.loyer@gmail.com>
2018-08-08 09:56:07 +0200

    Auto stash before merge of "master" and "origin/master"

--- sopramon-data-jpa/src/main/java/sopramon/IDAO/IDAOSopramon.java
+++ sopramon-data-jpa/src/main/java/sopramon/IDAO/IDAOSopramon.java
@@ -1,5 +1,34 @@
 package sopramon.IDAO;
 
-public interface IDAOSopramon {
+import java.util.List;
 
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import sopramon.model.Sopramon;
+
+	
+	public interface IDAOSopramon extends JpaRepository<Sopramon, Integer> {
+		
+		@Query("select p from Produit p where p.nom = :Planche")
+		public Sopramon findUnProduit(@Param("Planche") String libelle);
+		
+		public List<Sopramon> findByPrixBetween(double prix1, double prix2);
+		
+		public Sopramon save (Sopramon entity);
+		
+		public List<Sopramon> findByNom(String libelle);
+		
+		public List<Sopramon> findByPrix(String libelle);
+
+		public Sopramon findFirstByNom(String libelle);
+
+		public List<Sopramon> findTop10ByNomContainingOrderByIdDesc(String libelle);
+
+		public int countByNomContaining(String libelle);
+		
+		
+		
+	}
+	 
