49e9f700446dcc92251ee4cce72b9467086565b8
Unknown <briac.loyer@gmail.com>
2018-08-08 11:02:27 +0200

    idao

--- sopramon-data-jpa/src/main/java/sopramon/IDAO/IDAOCombat.java
+++ sopramon-data-jpa/src/main/java/sopramon/IDAO/IDAOCombat.java
@@ -1,8 +1,32 @@
 package sopramon.IDAO;
 
-import sopramon.IDAO.IDAO;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
 import sopramon.model.Combat;
 
-public interface IDAOCombat extends IDAO<Combat>{
+
+public interface IDAOCombat extends JpaRepository<Combat, Integer> {
+	
+	@Query("select c from combat c where c.id = :1")
+	public Combat findUnCombat(@Param("1") int combatid);
+	
+//	public List<Sopramon> findByPrixBetween(double prix1, double prix2);
+	
+	public Combat save (Combat entity);
+	
+	public List<Combat> findById(int combatid);
+	
+	//public List<Sopramon> findByPrix(String libelle);
+
+	public Combat findFirstById(int combatid);
+
+	public List<Combat> findTop10ByIdContainingOrderByIdDesc(int combatid);
+
+	public int countByIdContaining(int combatid);
 
 }
--- sopramon-data-jpa/src/main/java/sopramon/IDAO/IDAOUtilisateur.java
+++ sopramon-data-jpa/src/main/java/sopramon/IDAO/IDAOUtilisateur.java
@@ -1,8 +1,30 @@
 package sopramon.IDAO;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import sopramon.IDAO.IDAO;
 import sopramon.model.Utilisateur;
 
 public interface IDAOUtilisateur extends IDAO<Utilisateur>{
+
+	@Query("select c from combat c where c.id = :1")
+	public Utilisateur findUnCombat(@Param("") String nom);
+	
+//	public List<Sopramon> findByPrixBetween(double prix1, double prix2);
+	
+	public Utilisateur save (Utilisateur entity);
+	
+	public List<Utilisateur> findByNom(String nom);
+	
+	//public List<Sopramon> findByPrix(String libelle);
+
+	public Utilisateur findFirstByNom(String nom);
+
+	public List<Utilisateur> findTop10ByNomContainingOrderByIdDesc(String nom);
+
+	public int countByNomContaining(String nom);
 
 }
